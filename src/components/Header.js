2c6948a2d53c31530384fb2c7e2ed965ec322208
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-03 22:30:27 +0200

    refactoring 0.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className='flex w-full justify-between items-center h-12 text-white mr-4'>
       <div className='mx-4'>
-         <img className='w-32' src={logo} alt='' />
+         <img className='w-32 cursor-pointer' src={logo} alt='' />
       </div>
       <div className='bg-[#1c1c1e] flex flex-1 h-12 items-center rounded-3xl '>
          <div className='m-4 '>
@@ -16,12 +16,12 @@ const Header = () => {
          <input className='bg-transparent w-full outline-none text-[#eee] text-base '
           placeholder='Search collection,  items  or  user  ' />
       </div>
-      <div className='text-[#a1a5b0]'>
+      <div className='text-[#a1a5b0] cursor-pointer'>
          <span className='mx-2'>Drops</span>
          <span className='mx-2'>Marketplace</span>
          <span className='mx-2'>Create</span>
       </div>
-      <div className='flex items-center'>
+      <div className='flex items-center cursor-pointer'>
         <div className='mx-6  flex bg-[#1c1c1e] rounded-full p-3 justify-center items-center object-contain' >
           <img className='w-6' src={switchTheme} alt='' />
         </div>
